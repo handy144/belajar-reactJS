a261932bed32213cec0fbe4a724cc80d0fe38a4e
handy144 <papayapispot@gmail.com>
2025-07-04 22:21:04 +0700

    Update Navbar component

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,10 +8,6 @@ const Navbar = () => {
   // </div>
    <div className="bg-zinc-700 text-white relative rounded-b-2xl shadow-md shadow-zinc-800 py-1">
       <h1 className="logo font-bold text-3xl text-center">Todo</h1>
-      <div className=" flex flex-row absolute right-4 top-0.5 justify-between gap-4 items-center">
-        <p>Name</p>
-        <p className='rounded-full bg-blue-500 py-2 px-4'>0</p>
-      </div>
     </div>
   )
 }
